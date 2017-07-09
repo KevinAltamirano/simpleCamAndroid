ae5ae11106230cdb160a6733a678d6c92375d090
Kevin Altamirano <kevinaltperez@gmail.com>
2017-07-09 03:07:31 -0500

    AppCam
    Aplicacion Camara Basica Android

---
+++ simpleCam/app/src/main/java/kevinaltamirano/simplecam/MainActivity.java
@@ -0,0 +1,51 @@
+package kevinaltamirano.simplecam;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgimagen;
+    Button btnfoto;
+    static final int REQUEST_IMAGE_CAPTURE=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnfoto = (Button) findViewById(R.id.btnfoto);
+        imgimagen = (ImageView) findViewById(R.id.imgfoto);
+
+        btnfoto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                llamarIntent();
+            }
+        });
+    }
+
+    private void llamarIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent , REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgimagen.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+}
+
